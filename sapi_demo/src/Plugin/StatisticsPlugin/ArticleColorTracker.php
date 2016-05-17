5bda4eb31c4535d264654237d5f2d48d3b0e3fc0
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-05-17 16:11:55 +0300

    sapi-demo-stat-plugin: added initial demo sapi-statistics plugin for user-color-date tracking

---
+++ sapi_demo/src/Plugin/StatisticsPlugin/ArticleColorTracker.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\sapi_demo\Plugin\StatisticsPlugin;
+
+use Drupal\sapi\Plugin\StatisticsPluginInterface;
+use Drupal\sapi\Plugin\StatisticsPluginBase;
+use Drupal\sapi\StatisticsItemInterface;
+
+/**
+ * @StatisticsPlugin(
+ *  id = "article_colour_tracker",
+ *  label = "Track article colour views"
+ * )
+ */
+class ArticleColourTracker extends StatisticsPluginBase implements StatisticsPluginInterface {
+
+  /**
+   * EntityFieldQuery used to load the SAPI Data Items
+   *
+   * @var \Drupal\Core\Entity\Query\QueryFactory
+   */
+  protected $entityFieldQuery;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process(StatisticsItemInterface $item){
+
+    /**
+     * @var int $color
+     *   Term tid for the colour that we will track
+     */
+    $color = 1;
+    /**
+     * @var int $account
+     *   the uid for the user that we will track
+     */
+    $account = 1;
+    /**
+     * @var int $date
+     *  today's date, as a timestamp, but set to this morning.
+     */
+    $date = REQUEST_TIME;
+
+
+    /**  @var \Drupal\Core\Entity\Query\QueryFactory $queryFactory */
+    $queryFactory = \Drupal::service('entity.query');
+
+    /**
+     * @var array $results
+     *  retrieved using a \Drupal\Core\Entity\Query\QueryInterface
+     */
+    $results = $queryFactory->get('sapi_data')
+      ->condition('color', $color)
+      ->condition('user', $account)
+      ->condition('date', $date)
+      ->execute();
+
+    /** @var \Drupal\Core\Entity\EntityStorageInterface $entityStorageManager */
+    $entityStorageManager = \Drupal::service('entity_type.manager')->getStorage('sapi_data');
+
+    /** @var \Drupal\sapi_data\SAPIDataInterface $sapiData */
+
+    if (count($results)>0) {
+      $entity_id = reset(array_keys($results));
+      $sapiData = $entityStorageManager->load($entity_id);
+      $sapiData->set('frequency', $sapiData->get('frequency')+1);
+      $sapiData->save();
+    }
+    else {
+      $entityStorageManager->create([
+        'color' => $color,
+        'user' => $account,
+        'date' => $date,
+        'frequency' => 1
+      ])->save();
+    }
+
+  }
+
+}
