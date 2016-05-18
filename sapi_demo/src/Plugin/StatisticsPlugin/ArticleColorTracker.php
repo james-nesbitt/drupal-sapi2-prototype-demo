a4ed70fa855ad2a3db51e1af42f021275a3a9f20
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-05-18 13:49:02 +0300

    sapi-demo-plugin converted to use proper dep injection now also gets entity query from entity storage instead of service.

--- sapi_demo/src/Plugin/StatisticsPlugin/ArticleColorTracker.php
+++ sapi_demo/src/Plugin/StatisticsPlugin/ArticleColorTracker.php
@@ -2,6 +2,12 @@
 
 namespace Drupal\sapi_demo\Plugin\StatisticsPlugin;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\sapi\Plugin\StatisticsPluginInterface;
 use Drupal\sapi\Plugin\StatisticsPluginBase;
 use Drupal\sapi\StatisticsItemInterface;
@@ -19,7 +25,49 @@ use Drupal\sapi\StatisticsItemInterface;
  * @TODO currently route-id, item-action, and colour-field-name and logger id
  *   are all hardcoded strings.  These should be replaced or configured.
  */
-class ArticleColorTracker extends StatisticsPluginBase implements StatisticsPluginInterface {
+class ArticleColorTracker extends StatisticsPluginBase implements StatisticsPluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * Route match object as we only track on certain routes
+   *
+   * @protected \Drupal\Core\Routing\RouteMatchInterface $routeMatch
+   */
+  protected $routeMatch;
+
+  /**
+   * EntityTypeManager used to get entity storage for sapi_data items, which is
+   * used to create and edit sapi_data items from tracking.
+   *
+   * @protected Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Current user for tracking
+   *
+   * @protected Drupal\Core\Session\AccountProxyInterface $currentUser;
+   */
+  protected $currentUser;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $routeMatch, EntityTypeManagerInterface $entityTypeManager, AccountProxyInterface $currentUser) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->routeMatch = $routeMatch;
+    $this->entityTypeManager = $entityTypeManager;
+    $this->currentUser = $currentUser;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+
+      $container->get('current_route_match'),
+      $container->get('entity_type.manager'),
+      $container->get('current_user')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -31,22 +79,18 @@ class ArticleColorTracker extends StatisticsPluginBase implements StatisticsPlug
       return;
     }
 
-    /** @var \Drupal\Core\Routing\CurrentRouteMatch $route */
-    $route = \Drupal::routeMatch();
     // we will listen only to node canonical requests
-    if ($route->getRouteName()!='entity.node.canonical') {
+    if ($this->routeMatch->getRouteName()!='entity.node.canonical') {
       return;
     }
 
-    /** @var \Drupal\Core\Session\AccountProxyInterface $currentUser */
-    $currentUser = \Drupal::currentUser();
     // we will not track anonymous user
-    if ($currentUser->isAnonymous()) {
+    if ($this->currentUser->isAnonymous()) {
       return;
     }
 
     /** @var \Drupal\node\NodeInterface|null $currentNode */
-    $currentNode = $route->getParameter('node');
+    $currentNode = $this->routeMatch->getParameter('node');
     // parameter sanity check and color field check
     if (
         is_null($currentNode)
@@ -66,7 +110,7 @@ class ArticleColorTracker extends StatisticsPluginBase implements StatisticsPlug
      * @var int $account
      *   the uid for the user that we will track
      */
-    $account = $currentUser->id();
+    $account = $this->currentUser->id();
 
     /**
      * @var int $date
@@ -77,14 +121,27 @@ class ArticleColorTracker extends StatisticsPluginBase implements StatisticsPlug
     $date = gmdate(DATETIME_DATE_STORAGE_FORMAT, REQUEST_TIME);
 
     /**
+     * We use the entity storage a few times, so we shortcut it here.
+     * It is used to:
+     *   - give a query to search for existing items
+     *   - load an existing item if found
+     *   - create a new item if needed
+     *
+     * @var EntityStorageInterface $sapiDataStorage
+     */
+    $sapiDataStorage = $this->entityTypeManager->getStorage('sapi_data');
+
+    /**
      * Use EntityQuery to search for a matching color_frequency data
      * item "color-user-date".  This will tell us if we need to create
      * a new one, or update an existing one.
      *
      * @var array $results
-     *  retrieved using a \Drupal\Core\Entity\Query\QueryInterface
+     *
+     *  retrieved using a \Drupal\Core\Entity\Query\QueryInterface which came
+     *  from the Query Factory
      */
-    $results = \Drupal::entityQuery('sapi_data')
+    $results = $sapiDataStorage->getQuery()
       ->condition('type', 'color_frequency')
       ->condition('field_color', $color)
       ->condition('field_user', $account)
@@ -98,9 +155,7 @@ class ArticleColorTracker extends StatisticsPluginBase implements StatisticsPlug
       $entity_id = array_keys($results)[0];
 
       /** @var \Drupal\sapi_data\SAPIDataInterface $sapiData */
-      $sapiData = \Drupal::entityTypeManager()
-        ->getStorage('sapi_data')
-        ->load($entity_id);
+      $sapiData = $sapiDataStorage->load($entity_id);
 
       /** @var \Drupal\Core\Field\FIeldItemInterface $fieldFrequency */
       $fieldFrequency =& $sapiData->get('field_frequency')[0];
@@ -116,15 +171,13 @@ class ArticleColorTracker extends StatisticsPluginBase implements StatisticsPlug
       /** Creating a new tracking entity */
 
       /** @var \Drupal\sapi_data\SAPIDataInterface $sapiData */
-      $sapiData = \Drupal::entityTypeManager()
-        ->getStorage('sapi_data')
-        ->create([
-            'type' => 'color_frequency',
-            'field_color' => $color,
-            'field_user' => $account,
-            'field_date' => $date,
-            'field_frequency' => 1
-          ]);
+      $sapiData = $sapiDataStorage->create([
+        'type' => 'color_frequency',
+        'field_color' => $color,
+        'field_user' => $account,
+        'field_date' => $date,
+        'field_frequency' => 1
+      ]);
 
       if (!$sapiData->save()) {
         \Drupal::logger('sapi')->warning('Could not create SAPI data');
