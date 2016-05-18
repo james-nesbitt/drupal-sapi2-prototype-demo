8a7a9401ed8202ac7253f58350b70adbf008e92c
Raimonds Kalnins <raimonds.kalnins@wunderkraut.com>
2016-05-18 10:49:37 +0300

    Event subscriber added which sends StatisticsItem to Statistics API Dispatcher when "kernel.view" event is triggered.

---
+++ sapi_demo/src/EventSubscriber/RequestViewEventSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\sapi_demo\EventSubscriber;
+
+use Drupal\sapi\Dispatcher;
+use Drupal\sapi\StatisticsItem;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class RequestViewEventSubscriber
+ */
+class RequestViewEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The Statistics API dispatcher.
+   *
+   * @var \Drupal\sapi\Dispatcher $sapiDispatcher
+   */
+  protected $sapiDispatcher;
+
+  /**
+   * RequestViewEventSubscriber constructor.
+   *
+   * @param \Drupal\sapi\Dispatcher $sapiDispatcher
+   */
+  public function __construct(Dispatcher $sapiDispatcher) {
+    $this->sapiDispatcher = $sapiDispatcher;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  static function getSubscribedEvents() {
+    // Priority is set to 1 to avoid this listener from being stopped by other
+    // listeners.
+    // @see ContainerAwareEventDispatcher::dispatch()
+    $events[KernelEvents::VIEW][] = ['onRequestView', 1];
+    return $events;
+  }
+
+  /**
+   * Informs Statistics API dispatcher when controller outputs a value which is
+   * not a Response instance.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent $event
+   */
+  public function onRequestView(GetResponseForControllerResultEvent $event) {
+    try {
+      $this->sapiDispatcher->dispatch(new StatisticsItem('request_view', ''));
+    } catch (\Exception $e) {
+      watchdog_exception('sapi_demo', $e);
+    }
+  }
+
+}
