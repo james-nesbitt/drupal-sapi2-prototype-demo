5bf483dabb2db64316c5714e0f095bc1b955de7a
Irīna Puško <irina.pusko@wunderkraut.com>
2016-06-09 14:17:19 +0300

    JS Update detection on entities

--- sapi_demo/js/sapi_demo.js
+++ sapi_demo/js/sapi_demo.js
@@ -34,12 +34,31 @@
             /* send data with ajax */
             Drupal.sapi.send("Entity_interaction", action);
           }
-
-          /*console.log("Action: " + interaction, "; Entity: " + entity_type + ":" + entity_id + "; User: " + user);*/
         }
 
         else {
           /* here will go update and create detection */
+          var rest = path.substring(0, path.lastIndexOf("/") + 1);
+          var last = path.substring(path.lastIndexOf("/") + 1, path.length);
+          var entity = rest.split("/");
+          var entity_type = entity[0];
+          if(last === "edit") {
+            $("#edit-submit, ul[data-drupal-selector$='edit-save'] .form-submit").click(function() {
+              var interaction = "Update";
+              var entity_id = entity[1];
+              if (entity_type === "taxonomy") {
+                var action = {"action":interaction, "entity":entity_type + "_" + entity_id, "entity_id":entity[2], "user_id":user};
+                console.log(action);
+              }
+              else {
+                var action = {"action":interaction, "entity":entity_type, "entity_id":entity_id, "user_id":user};
+                console.log(action);
+              }
+
+              /* send data with ajax */
+              Drupal.sapi.send("Entity_interaction", action);
+            });
+          }
         }
 
       });
