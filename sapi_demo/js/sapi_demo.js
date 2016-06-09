ad7fddc69811cc4bcc9f3a2a1635cbeb7b2a0fb1
Irīna Puško <irina.pusko@wunderkraut.com>
2016-06-09 10:55:44 +0300

    Taxonomy term view case handling in js

--- sapi_demo/js/sapi_demo.js
+++ sapi_demo/js/sapi_demo.js
@@ -20,9 +20,16 @@
           var entity_id = entity[1];
 
           /* if no id value then don't count it */
-          if(typeof entity_id !== "undefined"){
-            var action = {"action":interaction, "entity":entity_type, "entity_id":entity_id, "user_id":user};
-            console.log(action);
+          if(typeof entity_id !== "undefined") {
+
+            if (entity_type === "taxonomy") {
+              var action = {"action":interaction, "entity":entity_type + "_" + entity_id, "entity_id":entity[2], "user_id":user};
+              console.log(action);
+            }
+            else {
+              var action = {"action":interaction, "entity":entity_type, "entity_id":entity_id, "user_id":user};
+              console.log(action);
+            }
 
             /* send data with ajax */
             Drupal.sapi.send("Entity_interaction", action);
