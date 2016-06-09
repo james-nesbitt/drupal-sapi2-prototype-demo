d15607f462baf942485c16c6f9d2857a738081dc
Irīna Puško <irina.pusko@wunderkraut.com>
2016-06-09 09:09:15 +0300

    js gets data from drupal settings and try to send by Drupal sapi library

--- sapi_demo/js/sapi_demo.js
+++ sapi_demo/js/sapi_demo.js
@@ -3,17 +3,41 @@
  * JavaScript library for the Statistics API (sapi) module.
  */
 
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
 
   "use strict";
   Drupal.behaviors.sapiDemoView = {
-    attach: function (context) {
+    attach: function (context, settings) {
       $(function() {
-        var action = $("link[rel='shortlink']").attr("href");
-        console.log(action);
+        var adminPage = drupalSettings.path.currentPathIsAdmin;
+        var path = drupalSettings.path.currentPath;
+        var user = drupalSettings.user.uid;
+
+        if (!adminPage) {
+          var interaction = "View";
+          var entity = path.split("/");
+          var entity_type = entity[0];
+          var entity_id = entity[1];
+
+          /* if no id value then don't count it */
+          if(typeof entity_id !== "undefined"){
+            var action = {"action":interaction, "entity":entity_type, "entity_id":entity_id, "user_id":user};
+            console.log(action);
+
+            /* send data with ajax */
+            Drupal.sapi.send("Entity_interaction", action);
+          }
+
+          /*console.log("Action: " + interaction, "; Entity: " + entity_type + ":" + entity_id + "; User: " + user);*/
+        }
+
+        else {
+          /* here will go update and create detection */
+        }
+
       });
 
     }
   };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, drupalSettings);
