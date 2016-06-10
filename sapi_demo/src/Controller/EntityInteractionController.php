56aed4a9325f60bb97b71c47f5522ec0bac5d81c
Lauris Igaunis <lauris.igaunis@wunderkraut.com>
2016-06-10 16:49:12 +0300

    Draft for future controller development with some comments

--- sapi_demo/src/Controller/EntityInteractionController.php
+++ sapi_demo/src/Controller/EntityInteractionController.php
@@ -8,13 +8,20 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Component\Plugin\PluginManagerInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\sapi\DispatcherInterface;
+use Drupal\sapi\ActionTypeInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
 
 /**
  * Class EntityInteractionController.
  *
  * @package Drupal\sapi_demo\Controller
  */
-class EntityInteractionController extends ControllerBase {
+class EntityInteractionController extends ControllerBase implements ContainerInjectionInterface{
 
   /**
    * @var \Symfony\Component\HttpFoundation\RequestStack $requestStack
@@ -23,13 +30,38 @@ class EntityInteractionController extends ControllerBase {
   protected $requestStack;
 
   /**
+   * @var \Drupal\sapi\DispatcherInterface $sapiDispatcher
+   *  use to receive action items
+   */
+  protected $sapiDispatcher;
+
+  /**
+   * The statistics action type plugin manager which will be used to create sapi
+   * items to be passed to the dispatcher
+   *
+   * @var \Drupal\Component\Plugin\PluginManagerInterface $sapi_action_type_manager
+   */
+  protected $sapi_action_type_manager;
+
+  /**
+   * Symfony Container which we may use to convert arguments to services
+   *
+   * @var \Symfony\Component\DependencyInjection\ContainerInterface $container
+   */
+  protected $container;
+
+  /**
    * EntityInteractionController constructor.
    *
    * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
-
+   * @param \Drupal\sapi\DispatcherInterface $sapiDispatcher
+   * @param \Drupal\Component\Plugin\PluginManagerInterface $sapi_action_type_manager
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
    */
-  public function __construct(RequestStack $requestStack, ContainerInterface $container) {
+  public function __construct(RequestStack $requestStack, DispatcherInterface $sapiDispatcher, PluginManagerInterface $sapi_action_type_manager, ContainerInterface $container) {
     $this->requestStack = $requestStack;
+    $this->sapiDispatcher = $sapiDispatcher;
+    $this->sapi_action_type_manager = $sapi_action_type_manager;
     $this->container = $container;
   }
 
@@ -39,6 +71,8 @@ class EntityInteractionController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('request_stack'),
+      $container->get('sapi.dispatcher'),
+      $container->get('plugin.manager.sapi_action_type'),
       $container
     );
   }
@@ -58,13 +92,29 @@ class EntityInteractionController extends ControllerBase {
     // Get current request.
     $request = $this->requestStack->getCurrentRequest();
     $configuration = $request->get('action');
+    $configurations = explode(":", $configuration);
+    //I splited string into peaces. Delimiter is ":"(my own customisation in sapi_demo.js file)
+
+
+
+    $actions = $this->sapi_action_type_manager->createInstance('entity_interaction', $configurations);
+    // never get further, because i get an error about "Expected string action in plugin generation. None provided."
+
+
+  /*  if (!($actions instanceof ActionTypeInterface)) {
+      throw new BadRequestHttpException('Action Parameter does not correspond to any know action type.');
+    }*/
 
 
     try {
 
-      \Drupal::logger('js_contr')->notice($configuration);
+      // Send to SAPI dispatcher.
+      //$this->sapiDispatcher->dispatch($action);
 
-      return new Response('OK', 200);
+      \Drupal::logger('js_contr')->notice($configuration);
+      //this logs $configuration set(not array)
+
+      return new JsonResponse('OK', 200);
     } catch (\Exception $e) {
       throw new HttpException(500, 'Internal Server Error', $e);
     }
